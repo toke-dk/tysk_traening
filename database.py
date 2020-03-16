b3e11a5a10eb2b00c6aa6ba10a325409ba84402d
Toke Friis <tokefriis@gmail.com>
2020-03-16 17:55:10 +0100

    laver tilfældige ord fra dbasen

--- database.py
+++ database.py
@@ -1,16 +1,22 @@
 import sqlite3
 import lister
+import random
 
 conn = sqlite3.connect('ord.db')
 
 c = conn.cursor()
 
-assert len(lister.l_da) == len(lister.l_ty)
-for i in range(len(lister.l_da) - 1):
-    sql = f"""INSERT INTO ord (dansk, tysk)
-                VALUES ('{lister.l_da[i]}', '{lister.l_ty[i]}')"""
-    print(sql)
-    c.execute(sql)
-conn.commit()
+#assert len(lister.l_da) == len(lister.l_ty)
+#for i in range(len(lister.l_da)):
+#    sql = f"""INSERT INTO ord (dansk, tysk)
+#                VALUES ('{lister.l_da[i]}', '{lister.l_ty[i]}')"""
+#    c.execute(sql)
+#conn.commit()
 
-print(c.fetchall())
+c.execute("""SELECT dansk
+            FROM ord""")
+
+resultat = c.fetchall()
+tysk_resultat = resultat[random.randint(0, len(lister.l_ty))][0]
+
+print(tysk_resultat)
